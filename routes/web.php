551d62c4a5bd695973c95c475b38e0a896534e30
Петър Илиев <164956056+PetarIliev22@users.noreply.github.com>
2026-08-09 23:58:59 +0300

    Remove DB timing route and unused import

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
-
-
-Route::get('/speed-db-double', function () {
-    $start1 = microtime(true);
-
-    DB::select('select 1');
-
-    $first = (microtime(true) - $start1) * 1000;
-
-    $start2 = microtime(true);
-
-    DB::select('select 1');
-
-    $second = (microtime(true) - $start2) * 1000;
-
-    return response()->json([
-        'first' => round($first, 2) . ' ms',
-        'second' => round($second, 2) . ' ms',
-    ]);
-});
-
 
 Route::middleware('guest')->controller(AuthController::class)->group(function () {
     Route::get('/login', 'show')->name('login');
@@ -37,13 +15,11 @@ Route::middleware('guest')->controller(AuthController::class)->group(function ()
         ->name('register.store');
 });
 
-
 Route::middleware('auth')->group(function () {
     Route::get('/home', function () {
         return view('pages.home');
     })->name('home');
 });
-
 
 Route::get('/', function () {
     return redirect()->route(
