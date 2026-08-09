7492e0382e122f79e421d7adbbf97b53ae82e2c2
Петър Илиев <164956056+PetarIliev22@users.noreply.github.com>
2026-08-09 23:50:50 +0300

    Measure two DB queries separately

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,22 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/speed-db', function () {
-    $start = microtime(true);
+Route::get('/speed-db-double', function () {
+    $start1 = microtime(true);
 
     DB::select('select 1');
 
+    $first = (microtime(true) - $start1) * 1000;
+
+    $start2 = microtime(true);
+
+    DB::select('select 1');
+
+    $second = (microtime(true) - $start2) * 1000;
+
     return response()->json([
-        'db' => round((microtime(true) - $start) * 1000, 2) . ' ms',
+        'first' => round($first, 2) . ' ms',
+        'second' => round($second, 2) . ' ms',
     ]);
 });
 
