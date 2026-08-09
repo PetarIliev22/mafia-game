75b7c470bfcf81e447c146eba07992b941508177
Петър Илиев <petyr2203@abv.bg>
2026-08-09 15:59:44 +0300

    Update home.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -26,7 +26,7 @@
         :name="$user->name"
         :rank="$user->rank['name']"
         :coins="$user->coins ?? 0"
-        :profile-url="route('profile')"
+        :profile-url="route('home', ['tab' => 'profile'])"
         :show-back="$activeTab !== 'home'"
         :back-url="route('home')"
         notifications-url="#"
