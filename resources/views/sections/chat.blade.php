6c3e275deb3c6aab162f1c9c101c0291ca43b91c
Петър Илиев <164956056+PetarIliev22@users.noreply.github.com>
2026-08-11 23:34:25 +0300

    Remove chat and games section views

--- resources/views/sections/chat.blade.php
+++
@@ -1,9 +0,0 @@
-<section class="section-chat">
-    <h1 class="h3 fw-bold mb-4">
-        Чат
-    </h1>
-
-    <p>
-        Тук ще бъде чатът.
-    </p>
-</section>
--- resources/views/sections/games.blade.php
+++
@@ -1,9 +0,0 @@
-<section class="section-games">
-    <h1 class="h3 fw-bold mb-4">
-        Игри
-    </h1>
-
-    <p>
-        Тук ще бъдат игрите.
-    </p>
-</section>
